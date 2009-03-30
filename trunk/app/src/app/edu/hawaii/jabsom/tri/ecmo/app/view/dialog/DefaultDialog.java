b350adb5ee8540641f12b364ceeff1caa99a2de5
ltanaka <ltanaka@6adee215-725e-4b28-87c1-af44a2b16021>
2009-03-30 20:11:54 +0000

    lt: Bugfix DefaultDialog blank after PatientDetailPanel
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@23 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/dialog/DefaultDialog.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/dialog/DefaultDialog.java
@@ -20,8 +20,8 @@ import king.lib.access.ImageLoader;
  * @since    August 13, 2008
  */
 public abstract class DefaultDialog extends AbstractDialog {
-  
-  /**
+   
+/**
    * Creates a dialog.
    * 
    * @param parent  The parent component of the dialog.
@@ -32,12 +32,14 @@ public abstract class DefaultDialog extends AbstractDialog {
    
     // set look
     setUndecorated(true);
+    //setBackground(new Color(0, 0, 0, 0)); // lt_add: Mac OS X specific setting for transparency
 
     // set content pane
     final Image backgroundImage = ImageLoader.getInstance().getImage("conf/gui/dialog.png");
     JPanel contentPane = new JPanel() {
+      @Override
       protected void paintComponent(Graphics g) {
-        // set antialised text
+        // set antialiased text
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -48,6 +50,8 @@ public abstract class DefaultDialog extends AbstractDialog {
         g.setColor(Color.BLACK);
         g.setFont(g.getFont().deriveFont(Font.BOLD, 20f));
         g.drawString(getTitle(), 35, 27);
+        
+        super.repaint(); // lt_add: displays dialog for mac jdk 1.5
       }
     };
     setContentPane(contentPane);
