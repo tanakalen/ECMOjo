676c15906924d7d96019005898c78b44d2f1d049
noblemaster <noblemaster@6adee215-725e-4b28-87c1-af44a2b16021>
2009-04-01 21:07:35 +0000

    Undid manager panel change.
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@28 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/ManagerPanel.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/ManagerPanel.java
@@ -143,7 +143,7 @@ public class ManagerPanel extends JPanel {
     java.awt.Component[] components = getComponents();
     for (java.awt.Component component: components) {
       if (componentType.isAssignableFrom(component.getClass())) {
-//        setComponentZOrder(component, getComponentCount() - 1); // Cycles once, TubeComp set to 19
+        setComponentZOrder(component, new Integer(getComponentCount() - 1));
         return;
       }
     }
