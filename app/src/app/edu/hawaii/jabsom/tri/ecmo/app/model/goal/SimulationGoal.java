ee927f61390e3bd716ee9e99f5e757121b0be771
Christoph Aschwanden <caschwan@hawaii.edu>
2009-04-22 20:36:52 +0000

    Simulation mode added.

---
+++ app/src/app/edu/hawaii/jabsom/tri/ecmo/app/model/goal/SimulationGoal.java
@@ -0,0 +1,55 @@
+package edu.hawaii.jabsom.tri.ecmo.app.model.goal;
+
+import edu.hawaii.jabsom.tri.ecmo.app.control.Action;
+import edu.hawaii.jabsom.tri.ecmo.app.model.Game;
+
+/**
+ * Simulation run without an actual goal. 
+ *
+ * @author   king
+ * @since    April 22, 2009
+ */
+public class SimulationGoal extends Goal {
+  
+  /**
+   * Handles an action.
+   * 
+   * @param action  The action to handle.
+   */
+  @Override
+  public void handle(Action action) {
+    // not used
+  }
+    
+  /**
+   * Returns true if the goal is reached.
+   * 
+   * @param game  The game.
+   * @return  True for reached.
+   */
+  @Override
+  public boolean isReached(Game game) {
+    return false;
+  }
+
+  /**
+   * Returns true if the "game" is "won". I.e. if the player succeeded.
+   * 
+   * @param game  The game.
+   * @return  True for won.
+   */
+  @Override
+  public boolean isWon(Game game) {
+    return false;
+  }
+
+  /**
+   * Returns the name of the component.
+   * 
+   * @return  The name.
+   */
+  @Override
+  public String getName() {
+    return "Simulation";
+  }
+}
