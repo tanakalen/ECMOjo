2d81813340c9639fd5a752f4615507cae139e7f2
Alex Wang <kinwang@hawaii.edu>
2009-05-12 23:05:28 +0000

    Updated lab images and lab request action.

--- app/src/app/edu/hawaii/jabsom/tri/ecmo/app/control/action/LabRequestAction.java
+++ app/src/app/edu/hawaii/jabsom/tri/ecmo/app/control/action/LabRequestAction.java
@@ -5,6 +5,8 @@ import edu.hawaii.jabsom.tri.ecmo.app.model.Game;
 import edu.hawaii.jabsom.tri.ecmo.app.model.comp.Component;
 import edu.hawaii.jabsom.tri.ecmo.app.model.comp.LabComponent;
 import edu.hawaii.jabsom.tri.ecmo.app.model.comp.Patient;
+import edu.hawaii.jabsom.tri.ecmo.app.model.comp.Patient.HeartFunction;
+import edu.hawaii.jabsom.tri.ecmo.app.model.comp.Patient.LungFunction;
 //import edu.hawaii.jabsom.tri.ecmo.app.model.comp.TubeComponent;
 import edu.hawaii.jabsom.tri.ecmo.app.model.lab.BloodGasLabTest;
 import edu.hawaii.jabsom.tri.ecmo.app.model.lab.ChemistryLabTest;
@@ -70,11 +72,51 @@ public class LabRequestAction extends Action {
       
       // fill in details
       Patient patient = game.getPatient();
-      labTest.setGluc(0);
-      labTest.setIonCa(0);
-      labTest.setK(0);
+      double weight = patient.getWeight();
+      
+      // lactate from patient
       labTest.setLactate(patient.getLactate());
-      labTest.setNa(0);
+
+      if (weight < 5) {
+        // glucose 50-80 range
+        labTest.setGluc(50 + (Math.random() * 5) + (patient.hashCode() % 25));
+        // ion ca 0.95-1.50 range
+        labTest.setIonCa(0.95 + (Math.random() * 0.55));
+        // K 3-7 range
+        labTest.setK(3 + (Math.random() * 4));
+        // Na 139-146 range
+        labTest.setNa(139 + (Math.random() * 5) + (patient.hashCode() % 2));
+      }
+      else if (weight < 10) {
+        // glucose 60-100 range
+        labTest.setGluc(60 + (Math.random() * 5) + (patient.hashCode() % 35));        
+        // ion ca 0.95-1.50 range
+        labTest.setIonCa(0.95 + (Math.random() * 0.55));
+        // K 3.5-5 range
+        labTest.setK(3.5 + (Math.random() * 1.5));
+        // Na 138-145 range
+        labTest.setNa(138 + (Math.random() * 5) + (patient.hashCode() % 2));     
+      }
+      else if (weight < 50) {
+        // glucose 70-105 range
+        labTest.setGluc(70 + (Math.random() * 5) + (patient.hashCode() % 30));                     
+        // ion ca 0.95-1.50 range
+        labTest.setIonCa(0.95 + (Math.random() * 0.55));
+        // K 3.5-5.0 range
+        labTest.setK(3.5 + (Math.random() * 1.5));
+        // Na 139-145 range
+        labTest.setNa(139 + (Math.random() * 6));
+      }
+      else {
+        // glucose 60-100 range
+        labTest.setGluc(60 + (Math.random() * 5) + (patient.hashCode() % 35));  
+        // ion ca 0.95-1.50 range
+        labTest.setIonCa(0.95 + (Math.random() * 0.55));
+        // K 3.5-4.5 range
+        labTest.setK(3.5 + (Math.random()));
+        // Na 136-146 range
+        labTest.setNa(136 + (Math.random() * 5) + (patient.hashCode() % 5));
+      }
       result = labTest;
     }
     else if (labTest.equals(HematologyLabTest.class)) {
@@ -82,23 +124,73 @@ public class LabRequestAction extends Action {
       
       // fill in details
       Patient patient = game.getPatient();
-      labTest.setFibrinogen(0);
+      double weight = patient.getWeight();
+
+      // Hct from patient Hgb*0.3
       labTest.setHct(patient.getHct());
+      // Hgb from patient 14.5-22.5 range
       labTest.setHgb(patient.getHgb());
-      labTest.setPlatelets(0);
-      labTest.setPt(0);
-      labTest.setPtt(0);
-      labTest.setWbc(0);
+      
+      if (weight < 5) {
+        // Fibrinogen 125-300 range
+        labTest.setFibrinogen(125 + (Math.random() * 5) + (patient.hashCode() % 70));
+        // platelets 100-478 * 10(9) range
+        labTest.setPlatelets(100 + (Math.random() * 5) + (patient.hashCode() % 373));
+        // PT 13-18 range
+        labTest.setPt(13 + (Math.random() * 5));
+        // PTT 80-100 range
+        labTest.setPtt(80 + (Math.random() * 5) + (patient.hashCode() % 15));
+        // WBC 9-30 * 10(9) range
+        labTest.setWbc(9 + (Math.random() * 5) + (patient.hashCode() % 16));
+      }
+      else if (weight < 10) {
+        // Fibrinogen 200-400 range
+        labTest.setFibrinogen(200 + (Math.random() * 5) + (patient.hashCode() % 195));
+        // platelets 100-400 * 10(9) range
+        labTest.setPlatelets(100 + (Math.random() * 5) + (patient.hashCode() % 295));
+        // PT 11-15 range
+        labTest.setPt(11 + (Math.random() * 4));
+        // PTT 80-100 range
+        labTest.setPtt(80 + (Math.random() * 5) + (patient.hashCode() % 15));
+        // WBC 5-19.5 * 10(9) range
+        labTest.setWbc(5 + (Math.random() * 4.5) + (patient.hashCode() % 10));
+      }
+      else if (weight < 50) {
+        // Fibrinogen 200-400 range
+        labTest.setFibrinogen(200 + (Math.random() * 5) + (patient.hashCode() % 195));
+        // platelets 100-400 * 10(9) range
+        labTest.setPlatelets(100 + (Math.random() * 5) + (patient.hashCode() % 295));
+        // PT 11-15 range
+        labTest.setPt(11 + (Math.random() * 4));
+        // PTT 80-100 range
+        labTest.setPtt(80 + (Math.random() * 5) + (patient.hashCode() % 15));
+        // WBC 5-16 * 10(9) range
+        labTest.setWbc(5 + (Math.random() * 5) + (patient.hashCode() % 6));
+      }
+      else {
+        // Fibrinogen 200-400 range
+        labTest.setFibrinogen(200 + (Math.random() * 5) + (patient.hashCode() % 195));
+        // platelets 100-400 * 10(9) range
+        labTest.setPlatelets(100 + (Math.random() * 5) + (patient.hashCode() % 295));
+        // PT 11-15 range
+        labTest.setPt(11 + (Math.random() * 4));
+        // PTT 80-100 range
+        labTest.setPtt(80 + (Math.random() * 5) + (patient.hashCode() % 15));
+        // WBC 4.5-11 * 10(9) range
+        labTest.setWbc(4.5 + (Math.random() * 4.5) + (patient.hashCode() % 2));
+      }
       result = labTest;
     }
     else if (labTest.equals(UltrasoundLabTest.class)) {
       UltrasoundLabTest labTest = new UltrasoundLabTest();
       
-      // FIX!!!!!!!!!! -> need real lab tests... (from scenario)!?
-      // TODO
-
-      labTest.setDescription("Abdomen, Ultrasound");
-      labTest.setImageName("CXR-normal.jpg");
+      // Create image name depending on the patient
+      Patient patient = game.getPatient();
+      String name = "ultra-";
+      name = name + ((patient.getHeartFunction() == HeartFunction.GOOD) ? "good-" : "bad-");
+      name = name + ((patient.getLungFunction() == LungFunction.GOOD) ? "good-" : "bad-");
+      labTest.setDescription("Ultrasound");
+      labTest.setImageName("xray-CXR-normal.png");
       
       labTest.setTime(game.getElapsedTime() / 1000);
       result = labTest;
@@ -106,11 +198,13 @@ public class LabRequestAction extends Action {
     else if (labTest.equals(XRayLabTest.class)) {
       XRayLabTest labTest = new XRayLabTest();
       
-      // FIX!!!!!!!!!! -> need real lab tests... (from scenario)!?
-      // TODO
-
+      // Create image name depending on the patient
+      Patient patient = game.getPatient();      
+      String name = "xray-";
+      name = name + ((patient.getHeartFunction() == HeartFunction.GOOD) ? "good-" : "bad-");
+      name = name + ((patient.getLungFunction() == LungFunction.GOOD) ? "good-" : "bad-");
       labTest.setDescription("Chest, X-Ray");
-      labTest.setImageName("CXR-normal.jpg");
+      labTest.setImageName("xray-CXR-normal.png");
       
       labTest.setTime(game.getElapsedTime() / 1000);
       result = labTest;
