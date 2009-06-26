f344a54a2c0b599d46f0d466cde44f23d6e7920f
noblemaster <noblemaster@6adee215-725e-4b28-87c1-af44a2b16021>
2009-06-26 21:27:49 +0000

    - Implemented the scenario loader loading lab images at scenario start. - Added and integrated the Echo lab test.
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@158 6adee215-725e-4b28-87c1-af44a2b16021

---
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/model/lab/EchoLabTest.java
@@ -0,0 +1,19 @@
+package edu.hawaii.jabsom.tri.ecmo.app.model.lab;
+
+/**
+ * The Echo lab test. 
+ *
+ * @author   king
+ * @since    June 26, 2009
+ */
+public class EchoLabTest extends ImagingLabTest {
+  
+  /**
+   * Returns the name of the component.
+   * 
+   * @return  The name.
+   */
+  public String getName() {
+    return "Echo";
+  }
+}
