aae90ee5593a58fb609e9df68a29d9bc61779f76
ltanaka <ltanaka@6adee215-725e-4b28-87c1-af44a2b16021>
2009-09-08 00:50:26 +0000

    lt: Linked flow to BP and HR
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@222 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/control/Updater.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/control/Updater.java
@@ -501,14 +501,33 @@ public final class Updater {
           }
         }
 
-        // SBP increase 10% if flow increase by 20mL/kg/min in VA AND bad heart
-        if ((mode == Mode.VA) && (heartFunction == HeartFunction.BAD)) {
-          double bpadjust = 10 / (20 * patient.getWeight()); // % change SBP for 1mL/min flow change
-          if (difference > 0) {
-            patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 + bpadjust));
+        // Link of flow to heart rate and blood pressure
+        if ((difference != 0) && (history.getFlow() != 0)) {
+          // SBP increase 10% if flow increase by 20mL/kg/min in VA AND bad heart
+          if ((mode == Mode.VA) && (heartFunction == HeartFunction.BAD)) {
+            double bpadjust = 10 / (20 * patient.getWeight()); // % change SBP for 1mL/min flow change
+            if (difference > 0) {
+              patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 + bpadjust));
+              patient.setHeartRate(patient.getHeartRate() - (0.1 * patient.getHeartRate() * difference));
+            }
+            else {
+              patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 - bpadjust));
+              patient.setHeartRate(patient.getHeartRate() + (0.1 * patient.getHeartRate() * difference));
+            }
           }
           else {
-            patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 - bpadjust));
+            double bpadjust = 0.005; // Constant to adjust bp (note: 5% change too big!!!)
+            // pump flow to patient heart rate & blood pressure
+            if (difference > 0) {
+              // flow higher then BP increases & heart rate decreases
+              patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 + bpadjust));
+              patient.setHeartRate(patient.getHeartRate() - (0.1 * patient.getHeartRate() * difference));
+            }
+            else {
+              // flow lower then blood pressure drops & heart rate increases
+              patient.setSystolicBloodPressure(patient.getSystolicBloodPressure() * (1 - bpadjust));
+              patient.setHeartRate(patient.getHeartRate() + (0.1 * patient.getHeartRate() * difference));
+            }
           }
         }
 
