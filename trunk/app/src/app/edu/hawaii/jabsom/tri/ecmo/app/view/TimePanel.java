32e44e7e1880979399dc33656f9b5f7a0f4fd568
noblemaster <noblemaster@6adee215-725e-4b28-87c1-af44a2b16021>
2009-10-08 23:50:34 +0000

    Updated the time display (blinking "BASELINE") for scenarios (init phase).
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@355 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/TimePanel.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/TimePanel.java
@@ -11,6 +11,7 @@ import javax.swing.JPanel;
 import king.lib.out.Error;
 
 import edu.hawaii.jabsom.tri.ecmo.app.model.Game;
+import edu.hawaii.jabsom.tri.ecmo.app.model.goal.BaselineGoal;
 
 /**
  * The time panel. 
@@ -21,7 +22,7 @@ import edu.hawaii.jabsom.tri.ecmo.app.model.Game;
 public class TimePanel extends JPanel implements Runnable {
 
   /** The font color. */
-  private final Color textColor = new Color(0.2f, 0.2f, 0.2f);
+  private final Color textColor = new Color(0.25f, 0.25f, 0.25f);
 
   /** The game. */
   private Game game;
@@ -42,8 +43,8 @@ public class TimePanel extends JPanel implements Runnable {
     setOpaque(false);
     
     // set size and location
-    setLocation(608, 15);
-    setSize(128, 50);
+    setLocation(400, 5);
+    setSize(400, 80);
   }
 
   /**
@@ -102,18 +103,52 @@ public class TimePanel extends JPanel implements Runnable {
     Graphics2D g2 = (Graphics2D)g;
     g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                         RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-    // set text properties
-    g.setColor(textColor);
-    g.setFont(g.getFont().deriveFont(Font.BOLD, 22f));
     
-    // draw text
-    int time = (int)(game.getElapsedTime() / 1000);
-    int hours = time / 3600;
-    int minutes = (time / 60) % 60;
-    int seconds = time % 60;
-    String text = hours + " : " + ((minutes < 10) ? "0" : "") + minutes
-                        + " : " + ((seconds < 10) ? "0" : "") + seconds;
-    g.drawString(text, 10, 34);
+    // text color
+    g.setColor(textColor);
+
+    // only show when we have a baseline type goal
+    if (game.getGoal() instanceof BaselineGoal) {
+      BaselineGoal goal = (BaselineGoal)game.getGoal();
+      boolean blink = (((System.nanoTime()) / 500000000) % 2) == 0;
+      
+      // draw depending on state
+      if (goal.isInit(game)) {
+        // blink "BASELINE" as needed
+        g.setFont(g.getFont().deriveFont(Font.BOLD, 32f));        
+        if (blink) {
+          g.setColor(Color.GRAY);
+          g.drawString("BASELINE", 39, 32);
+          g.drawString("BASELINE", 41, 32);
+          g.drawString("BASELINE", 39, 34);
+          g.drawString("BASELINE", 41, 34);
+          g.setColor(Color.YELLOW);
+          g.drawString("BASELINE", 40, 33);
+          g.setColor(textColor);
+        }       
+        
+        // draw time left as to when scenario actually starts
+        g.setFont(g.getFont().deriveFont(Font.BOLD, 18f));
+        int timeLeft = (int)((goal.getTimeInit() - game.getElapsedTime()) / 1000);
+        if (timeLeft < 0) {
+          timeLeft = 0;
+        }
+        g.drawString("Time to Scenario Start: " + timeLeft + "s", 40, 60);
+      }
+      else {
+        // draw time left when scenario ends
+        g.setFont(g.getFont().deriveFont(Font.BOLD, 18f));
+        int timeLeft = (int)((goal.getTimeLimit() - game.getElapsedTime()) / 1000);
+        if (timeLeft < 0) {
+          timeLeft = 0;
+        }
+        int hours = timeLeft / 3600;
+        int minutes = (timeLeft / 60) % 60;
+        int seconds = timeLeft % 60;
+        String text = hours + ":" + ((minutes < 10) ? "0" : "") + minutes
+                            + ":" + ((seconds < 10) ? "0" : "") + seconds;
+        g.drawString("Time Left: " + text, 163, 43);
+      }
+    }
   }
 }
