c2b1379590fceeb8904f251cb63edfe6504f8188
noblemaster <noblemaster@6adee215-725e-4b28-87c1-af44a2b16021>
2009-10-22 01:19:55 +0000

    Pnuts functionality added (work in progress).
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@377 6adee215-725e-4b28-87c1-af44a2b16021

---
+++ trunk/app/src/app/king/lib/script/model/Script.java
@@ -0,0 +1,73 @@
+package king.lib.script.model;
+
+/**
+ * A script.
+ *
+ * @author  noblemaster
+ * @since  October 21, 2009
+ */
+public class Script {
+
+  /** The ID. 0 for invalid. */
+  private long id;
+  
+  /** The language, such as Pnuts, Java, JavaScript etc. */
+  private String lang;
+  /** The source code. */
+  private String code;
+  
+  
+  /**
+   * Returns the ID.
+   *
+   * @return  The ID. 0 for invalid.
+   */
+  public long getId() {
+    return id;
+  }
+  
+  /**
+   * Sets the ID.
+   *
+   * @param id  The ID. 0 for invalid.
+   */
+  public void setId(long id) {
+    this.id = id;
+  }
+  
+  /**
+   * Returns the language.
+   *
+   * @return  The language, such as Pnuts, Java, JavaScript etc.
+   */
+  public String getLang() {
+    return lang;
+  }
+  
+  /**
+   * Sets the language.
+   *
+   * @param lang  The language, such as Pnuts, Java, JavaScript etc.
+   */
+  public void setLang(String lang) {
+    this.lang = lang;
+  }
+  
+  /**
+   * Returns the source code.
+   *
+   * @return  The source code.
+   */
+  public String getCode() {
+    return code;
+  }
+  
+  /**
+   * Sets the source code.
+   *
+   * @param code  The source code.
+   */
+  public void setCode(String code) {
+    this.code = code;
+  }
+}
