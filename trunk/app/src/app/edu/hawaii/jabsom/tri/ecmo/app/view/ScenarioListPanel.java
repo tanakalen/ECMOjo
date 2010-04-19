a01707600897e932a5356d018ce8f3251d655bc7
noblemaster <noblemaster@6adee215-725e-4b28-87c1-af44a2b16021>
2010-04-19 23:39:39 +0000

    Tooltip update.
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@540 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/ScenarioListPanel.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/ScenarioListPanel.java
@@ -82,9 +82,9 @@ public class ScenarioListPanel extends JPanel {
       public void rollover(int index) {
         if (index >= 0) {
           Scenario scenario = scenarios.get(index);
-          listPanel.setToolTipText("<html><table><tr><td width=\"250\" valign=\"top\"><b>" 
-              + scenario.getName() + "</b><br>" 
-              + scenario.getDescription() + "</td></tr></table></html>");
+          listPanel.setToolTipText("<u>" 
+              + scenario.getName() + "</u><br>" 
+              + scenario.getDescription() + "");
         }
         else {
           listPanel.setToolTipText(null);
