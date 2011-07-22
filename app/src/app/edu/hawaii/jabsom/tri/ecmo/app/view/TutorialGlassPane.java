335db41e09f108dffa6292bb1fb8dd387905bf37
Len Tanaka <tanakalen@gmail.com>
2011-07-22 01:07:15 +0000

    lt: Fix unable to clear dialog box in tutorial screens

--- app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/TutorialGlassPane.java
+++ app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/TutorialGlassPane.java
@@ -85,8 +85,8 @@ public class TutorialGlassPane extends JPanel implements AWTEventListener {
       int y = (int)point.getY();
       if ((x < 293) || (x > 800) || (y < 2) || (y > 95)) {
         setCursor(cursor);
-        if (((x < 364) || (x > 538) || (y < 370) || (y > 399))         // can still click dialog buttons
-            && ((x < 304) || (x > 584) || (y < 95) || (y > 343))) {   // can still click selected component
+        if (!(((x < 364) || (x > 538) || (y < 370) || (y > 399))         // can still click dialog buttons
+            && ((x < 304) || (x > 584) || (y < 95) || (y > 343)))) {   // can still click selected component
           if ((id == MouseEvent.MOUSE_CLICKED) || (id == MouseEvent.MOUSE_PRESSED) 
                                                || (id == MouseEvent.MOUSE_RELEASED)) {
             mouseEvent.consume();
