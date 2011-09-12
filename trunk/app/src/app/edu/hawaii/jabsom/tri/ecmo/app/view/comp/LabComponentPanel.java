d9e3230cba1820add22b0f37c8277ffe3020cb5f
ltanaka <ltanaka@6adee215-725e-4b28-87c1-af44a2b16021>
2011-09-12 07:13:47 +0000

    lt: ja[i18n] Adjusted Chem, Heme, Img buttons on main screen.
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@592 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/comp/LabComponentPanel.java
+++ trunk/app/src/app/edu/hawaii/jabsom/tri/ecmo/app/view/comp/LabComponentPanel.java
@@ -16,6 +16,7 @@ import edu.hawaii.jabsom.tri.ecmo.app.model.lab.ImagingLabTest;
 import edu.hawaii.jabsom.tri.ecmo.app.model.lab.LabTest;
 
 import king.lib.access.ImageLoader;
+import king.lib.util.Translator;
 
 /**
  * The lab component. 
@@ -55,21 +56,30 @@ public class LabComponentPanel extends ComponentPanel {
       setLocation(0, 464);
     }
     else if (labTest.equals(ChemistryLabTest.class)) {
-      normalImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-Chem.png");
-      rolloverImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-ChemRol.png");
-      selectedImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-ChemSel.png");
+      normalImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonChem[i18n]: conf/image/interface/game/Btn-Chem.png"));
+      rolloverImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonChemRol[i18n]: conf/image/interface/game/Btn-ChemRol.png"));
+      selectedImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonChemSel[i18n]: conf/image/interface/game/Btn-ChemSel.png"));
       setLocation(0, 488);
     }
     else if (labTest.equals(HematologyLabTest.class)) {
-      normalImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-Heme.png");
-      rolloverImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-HemeRol.png");
-      selectedImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-HemeSel.png");
+      normalImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonHeme[i18n]: conf/image/interface/game/Btn-Heme.png"));
+      rolloverImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonHemeRol[i18n]: conf/image/interface/game/Btn-HemeRol.png"));
+      selectedImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonHemeSel[i18n]: conf/image/interface/game/Btn-HemeSel.png"));
       setLocation(0, 512);
     }
     else if (labTest.equals(ImagingLabTest.class)) {
-      normalImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-Img.png");
-      rolloverImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-ImgRol.png");
-      selectedImage = ImageLoader.getInstance().getImage("conf/image/interface/game/Btn-ImgSel.png");
+      normalImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonImg[i18n]: conf/image/interface/game/Btn-Img.png"));
+      rolloverImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonImgRol[i18n]: conf/image/interface/game/Btn-ImgRol.png"));
+      selectedImage = ImageLoader.getInstance().getImage(
+          Translator.getString("image.ButtonImgSel[i18n]: conf/image/interface/game/Btn-ImgSel.png"));
       setLocation(0, 536);
     }
     else {
