9cbb185082368b27d588bb6c4c5dbcc894eeb9be
ltanaka <ltanaka@6adee215-725e-4b28-87c1-af44a2b16021>
2014-06-04 08:40:50 +0000

    Partial completion physiologic monitor vertical label i18n. (WIP)
    git-svn-id: https://ecmojo.svn.sourceforge.net/svnroot/ecmojo@755 6adee215-725e-4b28-87c1-af44a2b16021

--- trunk/app/src/main/java/edu/hawaii/jabsom/tri/ecmo/app/view/comp/PhysiologicMonitorComponentPanel.java
+++ trunk/app/src/main/java/edu/hawaii/jabsom/tri/ecmo/app/view/comp/PhysiologicMonitorComponentPanel.java
@@ -8,10 +8,12 @@ import java.awt.Image;
 import java.awt.RenderingHints;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
 
 import king.lib.access.ImageLoader;
 import king.lib.out.Error;
-
+import king.lib.util.Translator;
+import edu.hawaii.jabsom.tri.ecmo.app.gui.VerticalLabel;
 import edu.hawaii.jabsom.tri.ecmo.app.model.comp.PhysiologicMonitorComponent;
 
 /**
@@ -53,8 +55,21 @@ public class PhysiologicMonitorComponentPanel extends ComponentPanel implements 
     setLocation(0, 152);
     setSize(288, 105);
     
-    // set layout
     setLayout(null);
+    
+    // add label "Phy Monitor"
+    String result = Translator.getString("label.PhysMonitor[i18n]: Phy Monitor");
+    //TODO: i18n vertical versus horizontal orientation
+    //if lang.locale isEastern: JLabel resultLabel = new TextLabel()
+    //else: // lang.local isWestern
+    JLabel resultLabel = new VerticalLabel();
+    resultLabel.setText(result);
+    resultLabel.setFont(resultLabel.getFont().deriveFont(Font.BOLD, 12f));
+    resultLabel.setForeground(Color.WHITE);
+    resultLabel.setHorizontalAlignment(JLabel.CENTER);
+    resultLabel.setLocation(273, 0);
+    resultLabel.setSize(15, 105);
+    add(resultLabel);
   }
 
   /**
