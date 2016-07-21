a5a4ede03e2015a405583ec838337ff81140cb61
Len Tanaka <tanakalen@gmail.com>
2016-07-20 16:20:56 -1000

    Updated self-signing certificate and generate script.

--- trunk/app/src/main/resources/jarsign/generate.bat
+++ trunk/app/src/main/resources/jarsign/generate.bat
@@ -1,1 +1,8 @@
+# Generate local cert and keystore
 keytool -genkey -keystore keystore -keyalg rsa -dname "CN=ECMO Project, OU=Telehealth Research Institute, O=John A. Burns School of Medicine, L=Honolulu, ST=HI,C=US" -alias kalias -validity 3650 -keypass kpassword -storepass kpassword
+
+# Generate developer cert and keystore renew every 2y
+keytool -genkey -keystore %keystore%.jks -keyalg EC -dname "CN=ECMOjo Developer, OU=Telehealth Research Institute, O=John A. Burns School of Medicine, L=Honolulu, ST=HI,C=US" -alias ecmojo_dev -validity 730
+
+# Export self-signed certificate for import into trust store
+keytool -export -alias ecmojo_dev -sigalg EC -file ecmojo_app.cer -keystore %keystore%.jks
